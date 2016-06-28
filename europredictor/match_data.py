4cd8519366dd7f34cdcee0a72953908ce7de5856
philipp <phil9797@googlemail.com>
2016-06-29 01:09:00 +0200

    fix bugs

--- europredictor/match_data.py
+++ europredictor/match_data.py
@@ -1,39 +1,43 @@
 import requests
 from datetime import datetime
+from settings import BASE_DIR
 import json
+import os
 
-with open("/data/matchinfo.json", "w") as file:
+json_path =  BASE_DIR + "/europredictor/data/matchinfo.json"
+
+with open(json_path) as file:
     json_str = file.read()
     js_dict = json.loads(json_str)
 fixtures = js_dict['fixtures']
 
 def _is_after(isotime):
-    time = datetime.strptime(match['date'], "%Y-%m-%dT%H:%M:%S")
+    time = datetime.strptime(match['date'], "%Y-%m-%dT%H:%M:%SZ")
     return time > datetime.now()
 
 class Match:
     def __init__(self, json):
-        self.start_date = datetime.strptime(json['date'], "%Y-%m-%dT%H:%M:%S")
+        self.start_date = datetime.strptime(json['date'], "%Y-%m-%dT%H:%M:%SZ")
         self.status = json['status']
         self.home_team = json['homeTeamName']
         self.away_team = json['awayTeamName']
-        self.home_team_goals = json["goalsHomeTeam"]
-        self.away_team_goals = json["goalsAwayTeam"]
+        self.home_team_goals = json['result']["goalsHomeTeam"]
+        self.away_team_goals = json['result']["goalsAwayTeam"]
         
 def get_all_matches():
     return [Match(match) for match in fixtures]
     
 def get_past_matches():
-    return [Match(match) for match in fixtures if match.start_date < datetime.now()]
+    return [match for match in get_all_matches() if match.start_date < datetime.now()]
     
 def get_future_matches():
-    return [Match(match) for match in fixtures if match.start_date > datetime.now()]
+    return [match for match in get_all_matches() if match.start_date > datetime.now()]
 
 def update_info():
     global fixtures
     url = "http://api.football-data.org/v1/soccerseasons/424/fixtures"
     request = requests.get(url)
-    with open("/data/matchinfo.json", "w") as file:
+    with open(json_path, "w") as file:
         file.write(request.text)
     fixtures = request.json()
     
--- tests/test_match_data.py
+++ tests/test_match_data.py
@@ -1,15 +1,15 @@
 import unittest
 from datetime import datetime
-from europredictor.match_data import get_all_matches, Match
+from europredictor.match_data import get_all_matches, Match, get_future_matches
 
 
 class FilterTestCase(unittest.TestCase):
 
     def test_all_matches(self):
         matches = get_all_matches()
-        assertIsInstance(matches[0], Match)
+        self.assertIsInstance(matches[0], Match)
         
         
     def test_future_matches(self):
         matches = get_future_matches()
-        assertTrue(all([match.start_date > datetime.now() for match in matches]))
+        self.assertTrue(all([match.start_date > datetime.now() for match in matches]))
