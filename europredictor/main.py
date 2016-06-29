6024ebe1ec00a67a6723f61288051be3903dc5d0
Philipp Horn <philipp.horn4@gmail.com>
2016-06-29 14:41:10 +0000

    update front end

--- europredictor/main.py
+++ europredictor/main.py
@@ -1,6 +1,7 @@
-from flask import Flask, request, render_template, abort
+from flask import Flask, request, render_template, abort, url_for, redirect
 from flask_bootstrap import Bootstrap
 from match_data import get_all_teams
+from datetime import datetime
 #from graphing import sentiment_rolling_averages
 
 
@@ -9,21 +10,29 @@ Bootstrap(app)
 
 @app.route("/")
 def home():
-    return render_template('test.html', teams = get_all_teams())
+    return redirect(url_for("graph"))
 
 @app.route("/graph/")
 def graph():
     """page for showing a custom graph, specified in the GET-headers"""
-    print request.args.getlist("countries")
-    print "-------"
-    countries = request.args.getlist("countries")
-    #start = request.args.get("start", None)
-    #end = request.args.get("end", None)
-    if not countries:
-        abort(400)
-    data_points = sentiment_rolling_averages(countries, start, end)
+    graph_keys = {"startMonth", "endMonth", "startDay", "endDay", "countries"}
+    if graph_keys.intersection(request.args.keys()) == graph_keys:
+        countries = request.args.getlist("countries")
+        start_date, end_date = _get_times()
+        #data_points = sentiment_rolling_averages(countries, start, end)
 
-    return render_template('test.html')
+    return render_template('custom_graph.html', teams = get_all_teams())
+
+def _get_times():
+    # todo: add validation, support for multiple formats
+    s_month = request.args.get("startMonth", None)
+    s_day = request.args.get("startDay", None)
+    s_date = datetime.strptime(s_month + s_day + "2016", "%m%d%Y")
+    e_month = request.args.get("endMonth", None)
+    e_day = request.args.get("endDay", None)
+    e_date = datetime.strptime(e_month + e_day + "2016", "%m%d%Y")
+    return s_date, e_date
+
 
 if __name__ == "__main__":
     app.run(debug=True)
