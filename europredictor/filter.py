6e2ed0480c2fdb7f3a68fee29c09b6b932d7763c
philipp <phil9797@googlemail.com>
2016-07-01 10:57:47 +0200

    remove unused filter function

--- europredictor/filter.py
+++
@@ -1,8 +0,0 @@
-from .keywords import keywords
-import re
-    
-def filter_by_body(comments):
-    one_country = [comment for comment in comments if len(comment.countries) == 1]
-    mult_country = [comment for comment in comments if len(comment.countries) > 1]
-    return one_country, mult_country
-
