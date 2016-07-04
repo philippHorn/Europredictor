fc280a05fdf1cd67ad312b0fd17f331ef80adec7
philippHorn <phil9797@googlemail.com>
2016-07-04 17:59:10 -0400

    improve graphs by adding a rolling mean

--- europredictor/graphing.py
+++ europredictor/graphing.py
@@ -5,19 +5,16 @@ from db_IO import read_to_dataframe
 from bokeh.embed import components
 from flask import abort
 
-def sentiment_rolling_averages(countries = None, interval = 'hour', start_time = 0, end_time = None):
-    '''
+def prepare_df(countries = None, start_time = 0, end_time = None):
+    '''reads and prepares dataframe'''
 
-
-
-    '''
     if end_time is None:
         end_time = datetime.now()
         
     # Creat the dataframe from the database
     df = read_to_dataframe(countries = countries, average = False, start_time = start_time, end_time = end_time)
     if df.empty:
-        abort(400)
+        return
         
     # Calculate the overall sentiment
     df['sentiment'] = df.pos_sentiment - df.neg_sentiment
@@ -29,22 +26,26 @@ def sentiment_rolling_averages(countries = None, interval = 'hour', start_time =
     # Convert from unix timestamp to datetime
     df['timestamp'] = pd.to_datetime(df['timestamp'], unit='s')
     
-    
-    #This doesn't seem to be working yet - and it's not a rolling average.
-    #df = df.groupby([df["name"], df["timestamp"].dt.hour]).mean()
-    
-
-    return df
+    return  df
 
 def plot_countries(countries, start_time, end_time, smoothen = False):
     
-    df = sentiment_rolling_averages(countries = countries, start_time = start_time, end_time = end_time, interval = 'hour')
+    df = prepare_df(countries = countries, start_time = start_time, end_time = end_time)
+
+    if df is None:
+        abort(400)
+
     if smoothen:
         group = df.groupby('name')
+
         dfs = []
         for name, dataf in group:
-            dataf['sentiment'] = pd.rolling_mean(dataf['sentiment'], 20)
+            # window size should be proportional to the number of data points
+            # 30 was found to work nice through experimentation
+            w_size = int((dataf['sentiment'].size/30.0))    
+            dataf['sentiment'] = pd.rolling_mean(dataf['sentiment'], w_size)
             dfs.append(dataf)
+
         df = pd.concat(dfs)
         
     p = TimeSeries(df, x='timestamp', y='sentiment', color='name', width=1200)
--- europredictor/main.py
+++ europredictor/main.py
@@ -4,6 +4,7 @@ from keywords import keywords
 from datetime import datetime
 from graphing import plot_countries
 from match_data import get_past_matches, get_match
+from settings import SECRET_KEY
 
 HOUR_TSTAMP = 60 * 60.0
 
@@ -19,9 +20,12 @@ def matches():
         start_tstamp = float(start_date.strftime("%s"))
         match = get_match(start_date)
         countries = [match.away_team, match.home_team]
+
+        # get the graph with some extra time before and after the match
         graph = plot_countries(countries, 
-                               start_tstamp - HOUR_TSTAMP, 
-                               start_tstamp + HOUR_TSTAMP * 3)
+                               start_tstamp - HOUR_TSTAMP * 6, 
+                               start_tstamp + HOUR_TSTAMP * 5, 
+                               smoothen = True)
         
         
     matches = get_past_matches(grouped = True)
@@ -43,7 +47,7 @@ def graph():
         start_date, end_date = _get_times()
         s_timestamp = float(start_date.strftime("%s")) 
         e_timestamp = float(end_date.strftime("%s"))
-        graph = plot_countries(countries, s_timestamp, e_timestamp)
+        graph = plot_countries(countries, s_timestamp, e_timestamp, smoothen = True)
     return render_template('custom_graph.html', 
                             teams = sorted(keywords.keys()), 
                             active = "graph", 
@@ -69,6 +73,5 @@ if __name__ == "__main__":
     import os
     host = os.environ.get('IP', '0.0.0.0')
     port = int(os.environ.get('PORT', 8080))
-    app.secret_key = ''
+    app.secret_key = SECRET_KEY
     app.run(debug=True, host=host, port=port)
-    # app.run(debug=True)
