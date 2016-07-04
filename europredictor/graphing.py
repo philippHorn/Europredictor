6e45098340745f78fb508f916280f283657cdcf0
Philipp Horn <philipp.horn4@gmail.com>
2016-07-04 12:55:47 +0000

    make smoothening optional

--- europredictor/graphing.py
+++ europredictor/graphing.py
@@ -39,16 +39,14 @@ def sentiment_rolling_averages(countries = None, interval = 'hour', start_time =
 def plot_countries(countries, start_time, end_time, smoothen = False):
     
     df = sentiment_rolling_averages(countries = countries, start_time = start_time, end_time = end_time, interval = 'hour')
-    group = df.groupby('name')
-    dfs = []
-    # for name, dataf in group:
-    #     dataf['sentiment'] = pd.rolling_mean(dataf['sentiment'])
-    #     dfs.append(dataf)
-    
-    # #averages = [pd.rolling_mean(dataframe['sentiment'], 20) for name, dataframe in df.groupby('name')]
-    # df = pd.concat(dfs)
-    # print df
-    #df['sentiment'] = pd.rolling_mean(df['sentiment'], 20)
+    if smoothen:
+        group = df.groupby('name')
+        dfs = []
+        for name, dataf in group:
+            dataf['sentiment'] = pd.rolling_mean(dataf['sentiment'], 20)
+            dfs.append(dataf)
+        df = pd.concat(dfs)
+        
     p = TimeSeries(df, x='timestamp', y='sentiment', color='name', width=1200)
     return "\n".join(components(p))
 
